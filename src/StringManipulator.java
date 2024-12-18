b18eb5c018cd3debf973917c248c361264431f92
frankthebanker-esr <189669124+frankthebanker-esr@users.noreply.github.com>
2024-12-18 17:22:48 +0000

    Added basic files.

---
+++ src/StringManipulator.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class StringManipulator {
+	public static void main(String[]args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter the String: ");
+		String str = scanner.nextLine();
+		scanner.close();
+	}
+}
